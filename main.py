3087749740ad7387fcb8a3eeccef1554566875ed
Mike Hotchkiss <michael.hotchkiss@protonmail.com>
2020-10-11 12:39:06 -0400

    Euclid's algorithm

---
+++ main.py
@@ -0,0 +1,15 @@
+# Find the greatest common denominator of two numbers
+# using Euclid's algorithm
+
+def gcd(a, b):
+    while b != 0:
+        t = a
+        a = b
+        b = t % b
+
+    return a
+    # pass
+
+
+print(gcd(20, 8))
+print(gcd(60, 96))
